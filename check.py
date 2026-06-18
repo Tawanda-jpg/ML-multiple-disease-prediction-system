851af4f1116f62423c3d5edce179faf721eb1fa6
Tawanda Ndiyamba <tawandan02@gmail.com>
2026-06-18 22:21:35 +0200

    setting up project structure and directory

--- check.py
+++
@@ -1,9 +0,0 @@
-from src.training.config.settings import Settings
-
-settings = Settings()
-
-log_path = settings.log_path
-diabetes_path = settings.diabetes_dataset_path
-
-print(log_path)
-print(diabetes_path)
